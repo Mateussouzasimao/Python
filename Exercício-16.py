0e4eb587d93f173d31526b1071b2a09023b53c8d
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2022-12-27 17:35:12 -0300

    Update Exercício-16.py

--- Exercício-16.py
+++ Exercício-16.py
@@ -1,19 +1,38 @@
-from math import ceil
+from math import ceil,floor
 
 areaPintada = float(input('Digite o tamanho da área a ser pintada em m²: '))
 
-lataLitro = 18
-lataPreco = 80
-rendimento = 3
+#rendimento 1L - 6m²
+lataLitro = 18.0
+lataPreco = 80.0
+galaoLitro = 3.6
+galaoPreco = 25.0
 
 if areaPintada > 0:
-    litro = areaPintada/rendimento
+    litro = areaPintada/6
     latas = ceil(litro/lataLitro)
-       
+    galoes = ceil(litro/galaoLitro)
+    
+    
     valorPagar = latas*lataPreco
-        
     print(f'\nVocê precisará de {latas} latas e deverá pagar R${valorPagar}.')
-
-    print(f'Você precisara de {round(litro, 2)}L.')
+        
+    
+    valorPagar = galoes*galaoPreco
+    print(f'Você precisara de {galoes} galões e deverá pagar R${valorPagar}.')
+    
+    litroFolga = litro +(litro*0.1)
+    misturaLata = floor(litroFolga/lataLitro)
+    misturaGalao = ceil((litroFolga - (misturaLata * lataLitro)) / galaoLitro)
+    
+    if litroFolga%lataLitro != 0:
+        
+        valorPagarMistura1 = misturaLata*80
+        valorPagarMistura2 = misturaGalao*25
+        
+        valorPagTotal = valorPagarMistura1+valorPagarMistura2
+        
+        print(f'\nVocê precisara de {misturaLata} latas e {misturaGalao} galoes e deverá pagar R${valorPagTotal}.')
+    print(f'\nVocê precisara de {round(litro, 2)}L e {litroFolga - litro}L de folga.')
 else: 
-    print('\nDigite um valor maior que zero!')
+    print('\nDigite um valor maior que zero!')   
