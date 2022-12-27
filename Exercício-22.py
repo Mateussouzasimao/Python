13f218664c6affab6538f1f36c20a19f821cc8d7
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2022-12-27 17:30:40 -0300

    Create Exercício-22.py

---
+++ Exercício-22.py
@@ -0,0 +1,6 @@
+n1 = float(input('Por favor, insira o primeiro número: '))
+n2 = float(input('Por favor, insira o segundo número: '))
+n3 = float(input('Por favor, insira o terceiro número: '))
+
+
+print(f'\nO maior número entre os três é: {max(n1, n2, n3)}')
