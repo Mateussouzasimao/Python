346381d70e829cd4b858f2dbd2729c2aa5d2404f
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2022-12-30 13:46:12 -0300

    Update Exercício-49.py

--- Exercício-49.py
+++ Exercício-49.py
@@ -3,16 +3,8 @@ while True:
         entrada = int(input('Insira um valor de 1 a 10 para saber a tabuada: '))
         if entrada >= 1 and entrada <= 10:
             print(f'\nTabuada de {entrada}\n')
-            print(f'{entrada} X 1 = {entrada*1}')
-            print(f'{entrada} X 2 = {entrada*2}')
-            print(f'{entrada} X 3 = {entrada*3}')
-            print(f'{entrada} X 4 = {entrada*4}')
-            print(f'{entrada} X 5 = {entrada*5}')
-            print(f'{entrada} X 6 = {entrada*6}')
-            print(f'{entrada} X 7 = {entrada*7}')
-            print(f'{entrada} X 8 = {entrada*8}')
-            print(f'{entrada} X 9 = {entrada*9}')
-            print(f'{entrada} X 10 = {entrada*10}')
+            for i in range(1, 11):
+                print(f'{entrada} X {i} = {entrada * i}')
             break
         else:
             print('Informe apenas valores entre 1 e 10')
