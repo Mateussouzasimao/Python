a5338e96af975da48b3328d494afbad60d2234bd
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2022-12-30 13:42:53 -0300

    Create Exercício-47.py

---
+++ Exercício-47.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        abacate.append(int(input('Informe um número inteiro: ')))
+        abacate.append(int(input('Informe outro número inteiro: ')))
+        lista1 = range(min(abacate) + 1, max(abacate))
+        for i in lista1:
+            print(i)
+        break
+    except:
+        print('Informe apenas valores inteiros')
